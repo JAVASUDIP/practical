19b4f1390f0144daa7426d1e80127cc07bffb383
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-08-09 16:43:13 +0530

    Add files via upload

---
+++ CptHw1.java
@@ -0,0 +1,29 @@
+class CptHw1{
+
+  public static void main(String[]args){
+
+    //int a = 27;
+    //int b = 30;
+    //int c = 47;
+
+    //int sum = a+b+c;
+
+
+    //float a = 7.3f;
+    //float b = 8.5f;
+    //float c = 9.7f;
+
+    //float sum = a+b+c;
+
+    //double a = 10.997;
+    //double b = 71.695;
+   // double c = 150.669;
+   
+    //double sum = a+b+c;
+
+
+    System.out.println("the sum of these three number is " +sum);
+
+    }
+
+}
---
+++ adreass.java
@@ -0,0 +1,7 @@
+class Adreass{
+
+public static void main(String[]args){
+
+  System.out.println("Name-sudip Bhattacharjya,vill-Brindabanpur");
+}
+}
