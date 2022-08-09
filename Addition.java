8f8fc1e04e9f6830197e1a239dcf84403310f857
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-08-09 17:44:57 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,21 @@
+//class Addition{
+class Multiplication{
+public static void main(String[]args){   //main mathode 
+
+  System.out.println("Enter your Two number:"); 
+
+  int a = 5; //  initialize 1st local variable 
+  int b = 6;  //  initialize 2nd local variable
+
+
+ //int sum = a+b; // logic
+
+
+  //int subtraction = a-b; //logi
+
+   int multiplaction = a*b; // logic
+
+ System.out.println("Multiplication of the two number is" + Multiplication);
+
+  }
+}
---
+++ Multiplication.java
@@ -0,0 +1,26 @@
+//class Addition{
+ class Multiplication{
+
+public static void main(String[]args){   //main mathode 
+
+  System.out.println("Enter your Two number:"); 
+
+  int a = 50; //  initialize 1st local variable 
+  int b = 10;  //  initialize 2nd local variable
+
+
+ //int sum = a+b; // logic
+
+
+  //int subtraction = a-b; //logic
+
+   //int z = a * b; // logic
+
+ // division of two number
+
+  int division = a / b; //logic
+
+ System.out.println("Division of the two number is" + division);
+
+  }
+}
