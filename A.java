255d541133227b7c1d50d0dc6ef65f8e69a47ae1
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-08-09 16:56:20 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,15 @@
+class A {
+static int c = 30;
+int a = 10;
+
+ public static void main(String[]args){
+
+
+int b = 20 ;
+
+System.out.println(A.c);
+System.out.println(b);
+System.out.println(A.c);
+
+}
+}
