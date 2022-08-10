730fa233fc273d44181dd64d2a75968ee49100e6
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-08-10 19:04:01 +0530

    Add files via upload

---
+++ Areasq.java
@@ -0,0 +1,43 @@
+//(1) Write a java programer find out the area and perimeter of the Square. 
+
+ class Areasq{
+
+  public static void main (String[]args){
+
+  //int side = 5;             // Side of squarea.
+
+  //int area = side*side;    //Logic of area of Square.
+
+  //System.out.println("The area of square is:" +area);
+
+   //int perimeter = 4*side; // Logic of square perimeter.
+
+
+    //System.out.println("The perimeter of the square is:" +perimeter);
+
+
+  //(2) Write a java programer find out the area and perimeter of the Rectangle. 
+
+
+       int length = 6;  //Lenhth of Rectangle.
+
+       int width = 5;   // Width of Rectangle.
+
+       //int area_rectangle = length*width;  //Logic of area of rectangle.
+
+      //System.out.println("The area of the Rectangle is:" +area_rectangle);
+
+
+     int perimeter = 2*(length+width); //Logic of the perimeter of the Rectangle.
+
+       System.out.println("The perimeter of the Rectangle is:" +perimeter);
+
+
+
+
+
+
+
+
+  }
+}
