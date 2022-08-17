e8fff302f03c038ffde502ea0ae26310bf381383
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-08-17 22:32:47 +0530

    Add files via upload

---
+++ ArithmeticOp.java
@@ -0,0 +1,21 @@
+package hellojavaop;
+
+public class ArithmeticOp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10; //variable declear
+		int b = 20;  //variable seclear.
+		
+		System.out.println("a+b= is " + (a+b));
+		System.out.println("a-b= is"  + (a-b));
+		System.out.println("a*b= is " +(a*b));
+		System.out.println("a/b = is "+(a/b));
+		System.out.println("a%b = is "+(a%b));
+		
+		
+		
+	}
+
+}
