a5daeab5fa0adbbd6648f39b9c729e98319b35d8
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-09-20 13:56:25 +0530

    Add files via upload

---
+++ BoilerPlate.java
@@ -0,0 +1,37 @@
+class BoilerPlate
+{
+  public static void main(String[]args)
+{
+    byte code = 4;
+    System.out.println(code);
+   
+     long mobilenumber = 8537067566l;
+     
+    System.out.println(mobilenumber);
+    
+     int rupees=76600;
+
+      System.out.println(rupees);
+
+     double salaraypackage=534234.224;
+     System.out.println(salaraypackage);
+
+       char type='p';
+       System.out.println(type);
+        boolean verfied=false;
+    System.out.println(verfied);
+     String bio="cant talk watsapp only";
+
+      System.out.println(bio);
+
+
+     final short firstvaiableyear=2009;
+      System.out.println(firstvaiableyear);
+        
+
+       
+    
+
+
+       }
+}
