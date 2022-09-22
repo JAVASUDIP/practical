75ab7d25b833500e42d72c9ad1d7a5b6ced958b9
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-09-22 16:55:21 +0530

    Add files via upload

---
+++ Mathematic1.java
@@ -0,0 +1,20 @@
+class Mathematic1
+{
+   public static void main(String[]args)
+  {
+   int sum=4;
+   System.out.println("The number is increment is "+  ++sum);
+
+    int m=20;   // variable declear
+   int n=10;    // variable declear
+
+  System.out.println("the number M is " + m);     
+  System.out.println("the number M is " + --m);   // decrement operators
+  System.out.println("the number M is " + n);
+  System.out.println("the number M is " + --n);   // decrement operators
+  System.out.println("the number M is " + m--);    //-do-
+  System.out.println("the number M is " + n--);    //-do-
+  
+
+  }
+}  
