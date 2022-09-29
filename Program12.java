6e793be70c381db0a6d19b61acf58828947906b8
JAVASUDIP <110465798+JAVASUDIP@users.noreply.github.com>
2022-09-29 17:09:29 +0530

    Add files via upload

---
+++ Program12.java
@@ -0,0 +1,15 @@
+class Program12
+{
+  public static void signup(String FirstName,String Lastname,long mobilenumber,String Pword,String mailid,boolean gender)
+{ 
+   System.out.println("Select Your Dinner");
+}
+  
+{
+  public Static void main(String[]args)
+
+ {
+   System.out.println("main method");
+   signup("Sudip","Bhattacharjya","8537067566l","1123","sudipbhattacharjya@gmail.com",'M');
+  }
+}
